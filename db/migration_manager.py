d0d9601f479efe95a67d7e7421e6ac533d441080
nickstanchenkov <stanchenkovnikita@gmail.com>
2025-01-08 12:28:24 -0300

    Правка по деплою: добавил в реквайрементс нужные версии sqlalchemy

--- db/migration_manager.py
+++ db/migration_manager.py
@@ -3,10 +3,9 @@ import logging
 from sqlalchemy import create_engine, text, select
 from sqlalchemy.orm import sessionmaker
 
-from config import DATABASE_URL
 from db.models import Migration
 
-
+DATABASE_URL = 'postgresql://postgres:13579033@localhost:5432/AI_SDR_stage'
 engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(bind=engine)
 
